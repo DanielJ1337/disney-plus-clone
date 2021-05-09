4d28c9144d6d1f2cac04999359045e86f5b457fb
Daniel Juergens <djuergens561@gmail.com>
2021-05-09 18:50:46 +0200

    add login background image

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styled from "styled-components";
+
+export const Login = () => {
+  return <Container>Login</Container>;
+};
+
+const Container = styled.div`
+  position: relative;
+  height: calc(100vh-4.375rem);
+
+  background-image: url("/images/disney-plus-login.jpg");
+`;
